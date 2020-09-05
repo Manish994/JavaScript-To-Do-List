57461b3878d725b2cc1c3641993e51db29ee2419
Manish <manihs@gmail.com>
2020-09-05 13:19:26 +0545

    Working JS, AddToDo function worked!

--- js/app.js
+++ js/app.js
@@ -19,3 +19,26 @@ const options = {
 }
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-us", options);
+
+//Function add to do
+function addToDo(todo) {
+   const item = `<li class="item">
+   <i class="fa ${UNCHECK} co" job="complete" id="0"></i>
+   <p class="text">${todo}</p>
+   <i class="fa fa-trash-o de" job="delete" id="0"></i>
+   </li>`;
+   const position = "beforeend";
+   listElement.insertAdjacentHTML(position, item);
+}
+
+
+//add an item to the user interface when press Enter Key
+document.addEventListener("keyup", function (e) {
+   if (e.keyCode == 13) {
+      const todo = inputElement.value;
+      //check empty or not 
+      if (todo) {
+         addToDo(todo);
+      }
+   }
+})
