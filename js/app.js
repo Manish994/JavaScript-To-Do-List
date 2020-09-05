a4ef3b443d6a24ce440ecbdf5dd6b0d30cadba30
Manish <manihs@gmail.com>
2020-09-05 12:19:02 +0545

    Working JS, Upto Show Today Date.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 // CODE EXPLAINED channel
+
+//Select the elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const listElement = document.getElementById("list");
+const inputElement = document.getElementById("input");
+
+//Classes Names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//Show Today date
+const options = {
+   weekday: "long",
+   month: "short",
+   day: "numeric"
+}
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-us", options);
